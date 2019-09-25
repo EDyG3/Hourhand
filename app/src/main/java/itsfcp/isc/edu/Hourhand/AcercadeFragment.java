526e008980491f25551950544be5ef26c9ea6f6b
arturo <arturouc1997@gmail.com>
2019-09-25 16:29:05 -0500

    Prueba de GIT con un comentario.

--- app/src/main/java/itsfcp/isc/edu/Hourhand/AcercadeFragment.java
+++ app/src/main/java/itsfcp/isc/edu/Hourhand/AcercadeFragment.java
@@ -15,7 +15,7 @@ public class AcercadeFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    ///Prueba de comentario en Git JAUKMX
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;

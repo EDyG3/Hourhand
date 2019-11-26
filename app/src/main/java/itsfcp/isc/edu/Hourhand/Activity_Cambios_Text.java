40e4c0cb2b6368351fea529d8961a1872a1f0354
Slender5 <jafet.ramsell@gmail.com>
2019-11-26 01:01:02 -0500

    Añadidura de layouts de personalizacion

---
+++ app/src/main/java/itsfcp/isc/edu/Hourhand/Activity_Cambios_Text.java
@@ -0,0 +1,13 @@
+package itsfcp.isc.edu.Hourhand;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Cambios_Text extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__cambios__text);
+    }
+}

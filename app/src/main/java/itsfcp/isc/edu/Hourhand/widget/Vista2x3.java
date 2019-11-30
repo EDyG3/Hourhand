e43ae684047f45a2d11a0f4552c24d3d50ec6442
Slender5 <jafet.ramsell@gmail.com>
2019-11-30 18:18:47 -0500

    Mi list si se ve :v

--- app/src/main/java/itsfcp/isc/edu/Hourhand/widget/Vista2x3.java
+++ app/src/main/java/itsfcp/isc/edu/Hourhand/widget/Vista2x3.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.widget.RemoteViews;
 
+import itsfcp.isc.edu.Hourhand.ActivityPersonalizacion2;
 import itsfcp.isc.edu.Hourhand.Activity_Personalizacion;
 import itsfcp.isc.edu.Hourhand.R;
 import itsfcp.isc.edu.Hourhand.WidgetService;
@@ -36,7 +37,7 @@ public class Vista2x3 extends AppWidgetProvider {
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
 
-            Intent intent = new Intent(context, Activity_Personalizacion.class);
+            Intent intent = new Intent(context, ActivityPersonalizacion2.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent,0);
 
             RemoteViews views = new RemoteViews(context.getPackageName(),R.layout.vista2x3);
